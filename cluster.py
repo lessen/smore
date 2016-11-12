3e1a0793f005a952c236b7f4f30622aaa28655cc
Tim Menzies <tim@menzies.us>
2016-11-12 01:12:34 -0500

    saving

--- cluster.py
+++ cluster.py
@@ -6,46 +6,51 @@ from config import *
 from sample import *
 
 """
-tbl=o(rows=list of lists
-      pos = o(syms= index of nonNumeric indepedents
+tbl=o(rows= list of rows
+      cols= o(syms= index of nonNumeric indepedents
                nums= index of numeric indepdents
                objs= indenx of numeric classes)
 """
 
-def cluster( tbl ):
+%XXXX cache objective stats in t
+def cluster(rows, cols ):
   def cluster1(rows):
     t = o(has=rows, col=None,
           mid=None, left=None, right=None)
     if len(rows) >= small:
-      # find obj with max var
-      sd = 0
-      ## tod do combine the sort and divide as one thing
-      for col1 in pos.objs:
-        mid, sd1 = minSd([ row[col1] for row in rows ])
+      mid = int(len(rows) // 2)
+      sd,mid,col = 0,None,None
+      for col1 in cols.objs:
+        sd1,mid1 = zsd([row[col1] for row in rows])
         if sd1 > sd:
-          sd, t.col, t.mid = sd1, col, mid
-      # split
+          sd,mid,col = sd1,mid1,col1
       l,r = [],[]
       for row in rows:
-        what = l if row[ t.col ] <= t.mid else r
-        what.rows += [rows]
-      # recurse
-      t.left  = cluster1( l )
-      t.right = cluster1( r )
+        x     = row[col]
+        what  = l if x <= mid else r
+        what += [x]
+      left  = cluster1( l )
+      right = cluster1( r )
+      if len(rows) < 2*small:
+        if different(left,right):
+          t.mid  = mid
+          t.col  = col
+          t.left = left
+          t.right= right       
     return t
-  pos = tbl.pos
   small = max(len(tbl.rows)**THE.groups.cull,
                THE.groups.minGroup)
   return cluster1( tbl.rows)
     
- def midZsd(vals):
-    n   = len(vals)
-    lo  = int(0.05*n)
-    hi  = int(0.95*n)
-    lst = sorted(vals)[lo:hi]
-    mid = lst[ len(lst) // 2 ]
-    min = tmp[ 0]
-    max = tmp[-1]
-    zsd = Num([(x - min)/(max - min + 1e-32)
-               for x in lst]).sd()
-    return mid, zsd
+ def zsd(rows):
+   rows    = sorted(rows)
+   mid     = rows[ n // 2 ]
+   lo, hi  = int(0.05*n), int(0.95*n) + 1
+   min,max = rows[lo], rows[hi]
+   num     = Num()
+   for i in xrange(min,max):
+     x = rows[i][col]
+     num( (x - min)/(max - min + 1e-32) )
+   return num.sd(), rows[ n // 2 ]
+   
+def different(
