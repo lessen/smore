7ea192ebae00c31318dfddb8918aa2da4a257537
Tim Menzies <tim@menzies.us>
2016-11-20 00:15:55 -0500

    saving

--- cocomo.py
+++ cocomo.py
@@ -103,7 +103,7 @@ class Table:
             if len(row)> 0:
               yield row
               
-  def dist(i, j,k, what=["nums","syms"]):
+  def dist(i, j,k, what=["syms","nums"]):
     ds,ns = 0,1e-32
     for x in what:
       for y in i.cols[x]:
@@ -123,43 +123,88 @@ def median(lst):
 def percentiles(lst,p=4):
   all = sorted(lst)
   n   = len(lst)//p
-  return all[::n], all[3*n] - all[n]
-
-def second(lst): return lst[1]
-
-def p(x): return int(p*100)
+  return all[::n] 
 
 def sa(wantgot):
-  sampled = median( [got for _,got in wantgot] )
-  n       = len(wantgot)
-  denom   = sum([ abs(got - sampled) for    _,got in wantgot ]) / n
-  nom     = sum([ abs(got - want)    for want,got in wantgot ]) / n
-  return (nom / denom)
+  sampled = median( [want for want,_ in wantgot] )
+  denom   = sum([ abs(got - sampled) for    _,got in wantgot ]) 
+  nom     = sum([ abs(got - want)    for want,got in wantgot ]) 
+  return nom / (denom + 1e-32)
     
 def neighbors(row1, t):
+  second = lambda z:z[1]
   return map(second,
              sorted(
                [(t.dist(row1,row2), row2)
                 for row2 in t.rows ]))
-
+  
 def knn(row,t,k=5, goal=-1, combine = median):
   dists = neighbors(row,t)
   nears = dists[1:k+1]
   gots  = [x[goal] for x in nears]
   got   = combine(gots)
   return got
-      
+
+_  = None;  Coc2tunings = [[
+#              vlow  low   nom   high  vhigh  xhigh   
+# scale factors:
+'Flex',        5.07, 4.05, 3.04, 2.03, 1.01,    _],[
+'Pmat',        7.80, 6.24, 4.68, 3.12, 1.56,    _],[
+'Prec',        6.20, 4.96, 3.72, 2.48, 1.24,    _],[
+'Resl',        7.07, 5.65, 4.24, 2.83, 1.41,    _],[
+'Team',        5.48, 4.38, 3.29, 2.19, 1.01,    _],[
+# effort multipliers:        
+'acap',        1.42, 1.19, 1.00, 0.85, 0.71,    _],[
+'aexp',        1.22, 1.10, 1.00, 0.88, 0.81,    _],[
+'cplx',        0.73, 0.87, 1.00, 1.17, 1.34, 1.74],[
+'data',           _, 0.90, 1.00, 1.14, 1.28,    _],[
+'docu',        0.81, 0.91, 1.00, 1.11, 1.23,    _],[
+'ltex',        1.20, 1.09, 1.00, 0.91, 0.84,    _],[
+'pcap',        1.34, 1.15, 1.00, 0.88, 0.76,    _],[ 
+'pcon',        1.29, 1.12, 1.00, 0.90, 0.81,    _],[
+'plex',        1.19, 1.09, 1.00, 0.91, 0.85,    _],[ 
+'pvol',           _, 0.87, 1.00, 1.15, 1.30,    _],[
+'rely',        0.82, 0.92, 1.00, 1.10, 1.26,    _],[
+'ruse',           _, 0.95, 1.00, 1.07, 1.15, 1.24],[
+'sced',        1.43, 1.14, 1.00, 1.00, 1.00,    _],[ 
+'site',        1.22, 1.09, 1.00, 0.93, 0.86, 0.80],[ 
+'stor',           _,    _, 1.00, 1.05, 1.17, 1.46],[
+'time',           _,    _, 1.00, 1.11, 1.29, 1.63],[
+'tool',        1.17, 1.09, 1.00, 0.90, 0.78,    _]]
+
+def COCOMO2(project,  a = 2.94, b = 0.91, 
+                      tunes= Coc2tunings):
+  sfs,ems,kloc   = 0, 5 ,22        
+  scaleFactors, effortMultipliers = 5, 17
+  
+  for i in range(scaleFactors):
+    sfs += tunes[i][project[i]]
+    
+  for i in range(effortMultipliers):
+    j = i + scaleFactors
+    ems *= tunes[j][project[j]] 
+    
+  return a * ems * project[kloc] ** (b + 0.01*sfs)
+
 if __name__ == '__main__':
-  f       = "data/nasa93.csv"
-  t       = Table(file=f)
-  for k in [1,2,3,4,5]:
-    mres = []
-    wantgot = []
+  f = "data/nasa93_2000.csv"
+  t = Table(file=f)
+  p = lambda x:  int(x*100)
+  for k in [1,3,5,7]:
+    mres1 = []
+    mres2= []
+    wantgot1 = []
+    wantgot2 = []
     for row in t.rows:
-      want     = row[-1]
-      got      = knn(row, t, k=k)
-      mres += [abs(want-got)/want]
-      wantgot += [(want,got)]
-    mres = sorted(mres)
-    print(k, percentiles(mres))
-    #print(k, sa(wantgot), sum(mres)/len(t.rows))
+      want     = row[-8]
+      got1      = knn(row, t, goal=-8,k=k)
+      got2      = median(row[-8] for row in t.rows)
+      mres1 += [abs(want-got1)/want]
+      wantgot1 += [(want,got1)]
+      mres2 += [abs(want-got2)/want]
+      wantgot2 += [(want,got2)]
+    mres1 = sorted(mres1)
+    mres2 = sorted(mres2)
+    print(k, map(p,percentiles(mres1)))
+    #print(k, map(p,percentiles(mres2)))
+    #print(k, sa(wantgot), median(mres))
