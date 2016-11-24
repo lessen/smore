01346f98006f412ef2bbe64d01b0967b27c258c2
Tim Menzies <tim@menzies.us>
2016-11-24 18:08:05 -0500

    saving

--- cocomo.py
+++ cocomo.py
@@ -5,12 +5,21 @@ sys.dont_write_bytecode=True
 class Num:
   def __init__(i,txt,col):
     i.name,i.col, i.lo, i.hi = txt,col,1e32,-1e32
+    i.n, i.mu, i.m2 = 0,0,0
+    i.use = True
   def compile(i,x):
     return float(x)
   def add(i,x):
+    i.n += 1
+    delta = x - i.mu
+    i.mu += delta/i.n
+    i.m2 += delta*(x - i.mu)
     i.lo = min(x, i.lo)
     i.hi = max(x, i.hi)
     return x
+  def sd(i):
+    return 0 if i.n <= 2 else (i.m2/(i.n-1))**0.5
+  
   def norm(i,x):
     x = (x - i.lo) / (i.hi - i.lo + 1e-32)
     if x > 1: return 1
@@ -32,12 +41,21 @@ class Num:
   
 class Sym:
   def __init__(i,txt,col):
-    i.name,i.col, i.counts = txt,col,{}
+    i.n,i.name,i.col, i.counts = 0,txt,col,{}
+    i.use = True
   def compile(i,x):
     return x
   def add(i,x):
+    i.n += 1
     i.counts[x] = i.counts.get(x,0) + 1
     return x
+  def entropy(i):
+    e = 0
+    log2 = lambda x: math.log(x,2)
+    for count in i.counts.values():
+      p  = count/i.n
+      e -= p*log2(p)
+    return e
   def dist(i,x,y,miss="?"):
     if   x == miss and y == miss : return 0,0
     elif x == miss or  y == miss : return 1,1
@@ -57,23 +75,29 @@ class Table:
                     syms= ("=", Sym))
 
   def __init__(i,file=None):
-    i.rows,i.cols,i.all = [],{},[]
+    i.rows,i.cols,i.all, i.names = [],{},[], []
     i.col2cell, i.cell2col = {}, {}
     for key in Table.cols.keys():
       i.cols[key] = []
     if file:
       return i.create(i.lines(file))
 
+  def clone(i):
+    t = Table()
+    t.create([i.names[:]] + [row[:] for row in i.rows])
+    return t
+    
   def create(i,src):
     width = None
     for j,line in enumerate(src):
       if j == 0:
         width = len(line)
+        i.names=line
         i.header(line)
       else:
         assert width == len(line), "wanted %s cells" % width
         i.rows += [ i.compile(line) ]
-        
+
   def compile(i,line):
     for x in i.all:
       y = line[x.col] = x.compile( line[x.col] )
@@ -107,13 +131,14 @@ class Table:
             if len(row)> 0:
               yield row
               
-  def dist(i, j,k, what=["syms","nums"]):
+  def dist(i, j,k, what=["nums","syms"]):
     ds,ns = 0,1e-32
     for x in what:
       for y in i.cols[x]:
-        d,n  = y.dist(j[y.col], k[y.col], Table.missing)
-        ds  += d
-        ns  += n
+        if y.use:
+          d,n  = y.dist(j[y.col], k[y.col], Table.missing)
+          ds  += d
+          ns  += n
     return ds**0.5 / ns**0.5
 
 def median(lst):
@@ -130,8 +155,8 @@ def percentiles(lst):
   lo  = int(m*0.05)
   hi  = int(m*0.95)
   most= all[lo:hi]
-  q   = len(most)//4
-  return [most[0], most[q], most[q*2], most[q*3], most[-1]]
+  q   = len(most)//10
+  return [most[0], most[q], most[q*3], most[q*5], most[q*7], most[q*9], most[-1]]
 
 def sa(wantgot):
   sampled = median( [want for want,_ in wantgot] )
@@ -140,23 +165,66 @@ def sa(wantgot):
   return nom / (denom + 1e-32)
     
 def neighbors(row1, t):
-  second = lambda z:z[1]
-  return map(second,
-             sorted(
+  return sorted(
                [(t.dist(row1,row2), row2)
-                for row2 in t.rows ]))
-  
-def knn(row,t,k=5, goal=-1, combine = median):
-  dists = neighbors(row,t)
+                for row2 in t.rows ])
+
+def threeVal(t):
+  def swap(x):
+    if x in ['vl','l']     : return 'l'
+    if x in ['xh','vh','h']: return 'h'
+    return x
+  for i,row in enumerate(t.rows):
+    for sym in t.cols["syms"]:
+      row[sym.col] = swap(row[sym.col])
+  return t
+    
+def knn(row,t,k=5, goal=-1, combine = median,prep = lambda z:z,ent=None,sd=None):
+  t      = prep(t)
+  t      = prune(ent,sd)
+  second = lambda z:z[1]
+  dists = map(second, neighbors(row,t))
   nears = dists[1:k+1]
   gots  = [x[goal] for x in nears]
   got   = combine(gots)
   return got
 
+def knnC(row,t,k=5, goal=-1, combine = None,prep = lambda z:z,weight=1,ent=None,sd=None):
+  t      = prep(t)
+  t      = prune(t,ent,sd)
+  second = lambda z:z[1]
+  dists  = neighbors(row,t)
+  nears  = dists[1:k+1]
+  gots   = [(d,x[goal]) for d,x in nears]
+  denom,nom = 0,0
+  for d,got in gots:
+    d     += 1e-32
+    w      = (1/d)**weight
+    nom   += w*got
+    denom += w
+  return nom/denom
+
 def triangle(l):
   n     = len(l)
   denom = sum([(n-i) for i,x in enumerate(l)])
   return sum((n-i)*x for i,x in enumerate(l))/denom
+
+def prune(t,ent=None, sd=None):
+  if ent:
+    prune1(t,"syms",ent,lambda z: z.entropy)
+  if sd:
+    prune1(t,"nums",sd,lambda z: z.sd())
+  return t
+    
+def prune1(t,what,where,how):
+  all = t.cols[what]
+  for col in all:
+    all.use = False
+  good = sorted(all,key=how)
+  best = syms[where:] if where < 0 else syms[:where]
+  for col in best:
+    all.use = True
+  return t
     
 _  = None;  Coc2tunings = [[
 #              vlow  low   nom   high  vhigh  xhigh   
@@ -208,20 +276,29 @@ class Rx():
     p = lambda x:  int(x*100)
     i.mres = sorted(i.mres)
     print(map(p, percentiles( i.mres )),i.txt,d)
-               
-if __name__ == '__main__':
-  f = "data/nasa93_2000.csv"
-  g = -1
-  if len(sys.argv) > 1: f = sys.argv[1]
-  if len(sys.argv) > 2: g = int(sys.argv[2])
+
+def loo(f,g):
   t = Table(file=f)
   for k in [1,2,3,4,5]:
     print("#")
-    rs = [Rx("k=%snn" % k, lambda row: knn(row, t, goal=g, k=k)))
-         ,Rx("triangle",   lambda row: knn(row, t, goal=g, k=k, combine= triangle))
-         ,Rx("baseline",   lambda row: median(row[g] for row in t.rows))       
+    rs = [Rx("k=%snn"      % k,    lambda row: knn(row,  t.clone(), goal=g, k=k))
+          #,Rx("k=%snn.3val" % k,   lambda row: knn(row,  t.clone(), goal=g, k=k, prep=threeVal))
+          #Rx("k=%snn.w" % k, lambda row: knnC(row,  t.clone(), goal=g, k=k))
+          #,Rx("k=%snn.w2.3val" % k, lambda row: knn(row,  t.clone(), goal=g, k=k,prep=threeVal,ent=-5))
+          #,Rx("k=%snnC.w2.3val" % k, lambda row: knnC(row,  t.clone(), goal=g, k=k,prep=threeVal,ent=-5))
+         #,Rx("triangle",           lambda row: knn(row,  t.clone(), goal=g, k=k, combine= triangle))
+         #,Rx("baseline",           lambda row: median(row[g] for row in t.clone().rows))       
          ]
     for row in t.rows:
       for r in rs:
         r(row,row[g])
     for r in rs: r.report()
+  
+
+if __name__ == '__main__':
+  f = "data/nasa93_2000.csv"
+  g = -3
+  if len(sys.argv) > 1: f = sys.argv[1]
+  if len(sys.argv) > 2: g = int(sys.argv[2])
+  loo(f,g)
+
